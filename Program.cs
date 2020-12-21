8fb0411bd543f15e7b4aaf52384586756f306143
Phil <pmash2@users.noreply.github.com>
2020-12-20 20:50:28 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pmashbotCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
