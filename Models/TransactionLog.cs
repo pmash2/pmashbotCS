ce373dafff036fb1233d367ccf34c028d54a3086
Phil <pmash2@users.noreply.github.com>
2021-01-28 21:24:37 -0500

    Create new tables for tracking points

---
+++ Models/TransactionLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace pmashbotCS.Models
+{
+    public class TransactionLog
+    {
+        public int Id { get; set; }
+        public int Points { get; set; }
+        public string Receiver { get; set; }
+        public string Giver { get; set; }
+        public DateTime Date { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ Models/ViewerPoints.cs
@@ -0,0 +1,10 @@
+﻿
+namespace pmashbotCS.Models
+{
+    public class ViewerPoints
+    {
+        public int Id { get; set; }
+        public string Viewer { get; set; }
+        public int Points { get; set; }
+    }
+}
