fd298dc89a86c2523c6e43c917525110a3e00c70
Phil <pmash2@users.noreply.github.com>
2021-02-01 18:33:30 -0500

    Added validation and points to bet command

--- Commands/OddOrEven.cs
+++ Commands/OddOrEven.cs
@@ -1,4 +1,5 @@
-﻿using pmashbotCS.Models;
+﻿using pmashbotCS.Helpers;
+using pmashbotCS.Models;
 using System;
 
 namespace pmashbotCS.Commands
@@ -7,12 +8,35 @@ namespace pmashbotCS.Commands
     {
         public string Execute(string username, string[] args)
         {
+            if (args.Length < 3)
+            {
+                return $"@{username}, to play !bet, try !bet <odd/even> pointsToWager (!bet even 10)";
+            }
+
+            PointsManager mgr = new();
             bool betEven = args[1] == "even" ? true : false;
 
-            return PlayGame(betEven, username);
+            int wager = 0;
+            bool success = int.TryParse(args[2], out wager);
+            if (!success)
+            {
+                return $"@{username}, your wager must be a number!";
+            }
+
+            if (wager <= 0)
+            {
+                return $"@{username}, you must wager a postive amount";
+            }
+
+            if (mgr.GetPoints(username) < wager)
+            {
+                return $"{username}, you don't have enough points to make that bet";
+            }
+
+            return PlayGame(betEven, username, wager, mgr);
         }
 
-        public static string PlayGame(bool betEven, string userName)
+        public static string PlayGame(bool betEven, string userName, int wager, PointsManager mgr)
         {
             var rand = new Random();
             var number = rand.Next(0, 1000);
@@ -28,11 +52,13 @@ namespace pmashbotCS.Commands
             {
                 result += "YOU WIN!!!!";
                 didWin = true;
+                mgr.ChangePoints(userName, "", wager, "Won OddOrEven");
             }
             else
             {
                 result += "Oh no, you lose! :(";
                 didWin = false;
+                mgr.ChangePoints(userName, "", wager * -1, "Lost OddOrEven");
             }
 
             using (var context = new mashDbContext())
