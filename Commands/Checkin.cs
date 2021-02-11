19810092a2f3a3d3bfb23922ad432e1267d86cb4
Phil <pmash2@users.noreply.github.com>
2021-02-10 19:29:10 -0500

    Ensure viewers can only check in once per day

--- Commands/Checkin.cs
+++ Commands/Checkin.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TwitchLib.Client.Enums;
+using pmashbotCS.Repositories;
 
 namespace pmashbotCS.Commands
 {
@@ -20,10 +21,23 @@ namespace pmashbotCS.Commands
             {
                 return "Invalid check-in amount configured!";
             }
+
+            if (HasCheckedInToday(username))
+            {
+                return $"@{username}, nice try, but you already checked in today!";
+            }
+
             PointsManager mgr = new();
             mgr.ChangePoints(username, "pmashbot", pointsToGive, "Checked in!");
 
             return $"@{username}, thanks for checking in! You just got yourself {pointsToGive} points!";
+        }
+
+        private bool HasCheckedInToday(string username)
+        {
+            var lastCheckin = CheckInRepository.GetLastCheckIn(username);
+
+            return lastCheckin.Date == DateTime.Today.Date;
         }
     }
 }
---
+++ Repositories/CheckInRepository.cs
@@ -0,0 +1,25 @@
+﻿using pmashbotCS.Models;
+using System;
+using System.Linq;
+
+namespace pmashbotCS.Repositories
+{
+    public static class CheckInRepository
+    {
+        public static DateTime GetLastCheckIn(string username)
+        {
+            DateTime lastCheckIn;
+            using (var context = new mashDbContext())
+            {
+                lastCheckIn = context.TransactionLog
+                                     .Where(x => x.Notes == "Checked in!")
+                                     .Where(x => x.Receiver == username)
+                                     .OrderByDescending(x => x.Date)
+                                     .Select(x => x.Date)
+                                     .FirstOrDefault();
+            }
+
+            return lastCheckIn;
+        }
+    }
+}
